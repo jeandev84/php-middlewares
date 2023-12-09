76d5242a4b190912eadf025b575894b36938b926
Jean-Claude <jeanyao@ymail.com>
2023-12-09 11:56:49 +0300

    Refactoring

--- app/Middleware/Logging.php
+++ app/Middleware/Logging.php
@@ -44,3 +44,8 @@ final class Logging implements MiddlewareInterface
          return $response;
     }
 }
+
+/*
+[2023-12-09T11:56:03.197484+03:00] main.INFO: Request. {"request_id":"65742ba3302d1","request":{"Framework\\Component\\Http\\Request":{"requestId":"65742ba3302d1"}}} []
+[2023-12-09T11:56:03.198186+03:00] main.INFO: Application responded. {"request_id":"65742ba3302d1","response":{"Framework\\Component\\Http\\Response":{"status":"OK","body":"[]"}}} []
+*/
